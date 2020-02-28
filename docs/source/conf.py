8e320ca9ebe7a5f825412c50dac461f029d04e78
Alessandro Fiori <alessandro.fiori@gmailcom>
2020-02-28 13:00:37 +0100

    new realese

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -55,3 +55,5 @@ html_theme = 'nature'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+autodoc_mock_imports = ["paapi5_python_sdk"]
